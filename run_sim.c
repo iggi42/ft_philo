133d560c331eb1d956ebb44e079bd8664ab35b56
Felix Krüger <fkruger@students.42vienna.com>
2026-07-23 08:27:24 +0200

    experiment with sleep & interput mixture to make threads end

--- run_sim.c
+++ run_sim.c
@@ -32,6 +32,36 @@ static t_frk	*bring_the_cutlery(size_t n)
 	return (frks);
 }
 
+// returns true if the thread should end asap
+// call my_sleep(-1) to make my_sleep(x) return true forever
+// my_sleep(0) still waits a minimal hard coded default (42ns or smth)
+bool	my_sleep(t_timespan ms)
+{
+	static bool				should_exit_asp;
+	static bool				is_init;
+	static pthread_mutex_t	sleep_mut;
+
+	if (!is_init)
+	{
+		if (!pthread_mutex_init(&sleep_mut, NULL))
+			is_init = true;
+		else
+			return (true);
+	}
+	if (pthread_mutex_lock(&sleep_mut))
+		return (true);
+	if (ms == -1)
+		should_exit_asp = true;
+	if (should_exit_asp)
+		return (pthread_mutex_unlock(&sleep_mut), true);
+	pthread_mutex_unlock(&sleep_mut);
+	if (ms > 0)
+		usleep(ms * 1000);
+	else
+		usleep(42);
+	return (false);
+}
+
 void	philo_routine_eating(t_philo *me)
 {
 	t_frk	*fs[2];
@@ -50,15 +80,16 @@ void	philo_routine_eating(t_philo *me)
 				io_queue(log_forklift, me);
 				set_last_meal2now(me);
 				has_eaten = true;
+				if (my_sleep(0))
+					return ;
 				io_queue(log_eating, me);
-				usleep(me->c->t2eat * 1000);
+				my_sleep(me->c->t2eat);
 				putdown(fs[1]);
 			}
 			putdown(fs[0]);
-			if (has_eaten)
-				return ;
 		}
-		usleep(200);
+		if (has_eaten || my_sleep(0))
+			return ;
 	}
 }
 
@@ -73,9 +104,12 @@ void	*philo_routine(void *s)
 	while (me->c->max_meals <= 0 || meals < me->c->max_meals)
 	{
 		philo_routine_eating(me);
+		if (my_sleep(0))
+			break ;
 		meals++;
 		io_queue(log_sleeping, me);
-		usleep(me->c->t2nap * 1000);
+		if (my_sleep(me->c->t2nap))
+			break ;
 		io_queue(log_thinking, me);
 	}
 	turn_off_philo(me);
@@ -100,6 +134,7 @@ size_t	spawn_philos(t_philo_conf *c, t_philo *philos)
 		if (pthread_create(&(philos[i].thread_id), NULL, philo_routine,
 				&(philos[i])))
 			return (i);
+		pthread_detach(philos[i].thread_id);
 		i++;
 	}
 	return (i);
@@ -113,7 +148,6 @@ bool	handle_happy(t_philo *philo)
 {
 	if (philo->last_meal != -1)
 		return (false);
-	pthread_detach(philo->thread_id);
 	philo->thread_id = 0;
 	return (true);
 }
@@ -123,7 +157,6 @@ bool	handle_starved(t_philo *philo)
 {
 	if ((read_timer() - philo->last_meal) < philo->c->t2die)
 		return (false);
-	pthread_detach(philo->thread_id);
 	philo->thread_id = 0;
 	return (true);
 }
@@ -148,13 +181,13 @@ t_philo_event	phil_iterate(t_philo_conf *c, t_philo *philos)
 		{
 			result = NO_EVENT;
 			pthread_mutex_lock(&philos[i].last_meal_mutex);
-				// TODO error handling
+			// TODO error handling
 			if (handle_starved(&philos[i]))
 				result = (io_queue(log_died, &philos[i]), DEAD);
 			if (c->max_meals >= 0)
 				handle_happy(&philos[i]);
 			pthread_mutex_unlock(&philos[i].last_meal_mutex);
-				// TODO error handling
+			// TODO error handling
 			if (result == DEAD)
 				return (result);
 		}
@@ -171,6 +204,7 @@ void	wait4end(t_philo_conf *c, t_philo *philos)
 	result = NO_EVENT;
 	while (result == NO_EVENT)
 		result = phil_iterate(c, philos);
+	my_sleep(-1);
 }
 
 int	run_sim(t_philo_conf *c)
@@ -178,7 +212,7 @@ int	run_sim(t_philo_conf *c)
 	t_philo	*philos;
 	t_frk	*frks;
 
-	if (!c)
+	if (!c || my_sleep(0))
 		return (0);
 	frks = bring_the_cutlery(c->n_phil);
 	philos = new_philos(c, frks);
